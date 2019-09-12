99228ca1a7a5a7d9180fb9fe64b7358f7f878166
grzeli <qwerty@DESKTOP-LE4423C.localdomain>
2019-09-12 17:37:35 +0200

    first steps with hooks

---
+++ src/components/Ingredients/Ingredients.js
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
+import Search from './Search';
+
+const Ingredients = () => {
+  const [ingredients, setIngredients] = useState([]);
+
+  const addIngredientHandler = ingredient => {
+    setIngredients(prevIngredients => [...prevIngredients, { id: Math.random().toString(), ...ingredient}]);
+  }
+
+  const removeIngredientHandler = ingredientId => {
+    setIngredients(prevIngredients => prevIngredients.filter((ingredient) => ingredient.id !== ingredientId))
+  }
+
+  return (
+    <div className="App">
+      <IngredientForm onAddIngredient={addIngredientHandler} />
+
+      <section>
+        <Search />
+        <IngredientList ingredients={ingredients} onRemoveItem={removeIngredientHandler}/>
+        {/* Need to add list here! */}
+      </section>
+    </div>
+  );
+}
+
+export default Ingredients;
